915fb49baf103ce0419ef91221131b916562f575
Ryan <nananana@ba.com>
2013-07-18 08:00:51 +0000

    git

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
+var hello = new Buffer(fs.readFileSync('index.html'));
+var hello2 = hello.toString('utf8');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send("hello2");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
